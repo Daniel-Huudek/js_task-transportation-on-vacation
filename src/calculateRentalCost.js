68a5f84d18478fcdeb67b6ea25eec61ea35c26c0
Daniel-Huudek <139517145+Daniel-Huudek@users.noreply.github.com>
2025-05-17 17:14:55 -0300

    task done

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,23 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const basicDiscount = 3;
+  const mediumDiscount = 6;
+  const additionalDiscount = 7;
+
+  if (days === additionalDiscount) {
+    return 230;
+  }
+
+  if (days === mediumDiscount) {
+    return 220;
+  }
+
+  if (days === basicDiscount) {
+    return 100;
+  }
+
+  return 80;
 }
 
 module.exports = calculateRentalCost;
